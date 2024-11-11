27ca7c1b0d939e28da9a2026e2f71c49f4788f69
cfisbeck1 <cfisbec1@uwyo.edu>
2024-11-11 05:12:54 +0000

    finished lab

--- cosc1010-lab08.py
+++ cosc1010-lab08.py
@@ -1,8 +1,8 @@
-# Your Name Here
+# Connor Fisbeck
 # UWYO COSC 1010
 # Submission Date
-# Lab XX
-# Lab Section:
+# Lab 08
+# Lab Section: 15
 # Sources, people worked with, help given to:
 # your
 # comments
@@ -13,6 +13,34 @@
 # If they can't be converted return false
 # Other wise return the converted int or float 
 # Floats should only have one decimal point in them 
+
+def check_convert_str(string):
+    # check if string is an int or a float and converts the string to that type
+    # returns "null" if string wasnt converted
+    given = string
+    is_float = False
+    decimals = 0
+    for i in string:
+        if i == ".":
+            decimals += 1
+            if decimals == 1:
+                is_float = True
+            elif decimals > 1:
+                return False
+    if is_float == True:
+        str_float = float(string)
+        if type(str_float) == float:
+            return str_float
+        else:
+            return False
+    else:
+        str_int = int(string)
+        if type(str_int) == int:
+            return str_int
+        else:
+            return False
+
+
 
 
 print("*" * 75)
@@ -33,10 +61,49 @@ print("*" * 75)
 # m, b can be floats or integers
 # the bounds must be integers, if not return false
 
+def slope_intercept(m,b,xlower,xupper):
+    # calculate y values using point-slope formula
+    m = check_convert_str(m)
+    b = check_convert_str(b)
+    xlower = check_convert_str(xlower)
+    xupper = check_convert_str(xupper)
+    if xlower < xupper and type(xupper) == int and type(xlower) == int:
+        x = []
+        y = []
+        while xlower <= xupper:
+            x.append(xlower)
+            y.append(m * xlower + b)
+            xlower += 1
+        return f"x = {x}",f"y = {y}"
+    else:
+        return False
+
+
+
 # Create a while loop to prompt users for their input for the four variables
 # Exit on the word exit
 # Remember all inputs are strings, but the function needs ints or floats
 # Call your function and print the resulting list
+user_exit = False
+while user_exit == False:
+    m = input("Enter your slope m: ").lower()
+    if m == "exit":
+        user_exit = True
+        break
+    b = input ("Enter your y-intercept b: ").lower()
+    if b == "exit":
+        user_exit = True
+        break
+    xlower = input ("Enter your lower x bound as an integer: ").lower()
+    if xlower == "exit":
+        user_exit = True
+        break
+    xupper = input ("Enter your upper x bound as an integer: ").lower()
+    if xupper == "exit":
+        user_exit = True
+        break
+    print(slope_intercept(m,b,xlower,xupper))
+
 
 print("*" * 75)
 
@@ -48,3 +115,46 @@ print("*" * 75)
 # Create a loop like above to prompt the user for input for the three values
 # Create a second function that just does the square root operation 
     # If the number you are trying to take the square root of is negative, return null
+def sqrt(a,b,c):
+   # function to calculate square root portion of quadratic formula
+    a = check_convert_str(a)
+    b = check_convert_str(b)
+    c = check_convert_str(c)
+    result = (b**2 - 4*a*c)**(1/2)
+    if a == False or b == False or c == False:
+        return False
+    if result < 0:
+        return False
+    else:
+        return result
+
+
+
+def quadratic(a,b,c):
+    #calculate the quadratic formula
+    s = sqrt(a,b,c)
+    a = check_convert_str(a)
+    b = check_convert_str(b)
+    c = check_convert_str(c)
+    if s != False:
+        result1 = (-b + s)/(2*a)
+        result2 = (-b - s)/(2*a)
+        return f"x = {result1} and x = {result2}"
+    else:
+        return False
+
+user_exit = False
+while user_exit == False:
+    a = input("Enter a for quadratic formula: ").lower()
+    if a == "exit":
+        user_exit = True
+        break
+    b = input ("Enter b for quadratic formula: ").lower()
+    if b == "exit":
+        user_exit = True
+        break
+    c = input ("Enter c for quadratic formula: ").lower()
+    if c == "exit":
+        user_exit = True
+        break
+    print(quadratic(a,b,c))
